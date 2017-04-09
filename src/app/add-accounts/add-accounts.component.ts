9af15900ac82e97cc09e1a111fcf88bdd7430782
Ihor Henal <grom3198@gmail.com>
2017-04-10 00:11:35 +0300

    Added build directory to angular config and added vk login.

--- src/app/add-accounts/add-accounts.component.ts
+++ src/app/add-accounts/add-accounts.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../shared/common.service'
 import { Social } from '../shared/CSocial'
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs";
+import {error} from "util";
+
+
 
 @Component({
   selector: 'app-add-accounts',
@@ -11,13 +16,13 @@ export class AddAccountsComponent implements OnInit {
   title: string = "Add accounts";
   subtitle: string = "The more accounts you add, the more comfortable it will be for you";
   socials: Social[];
-  constructor(private service: CommonService) { }
+  constructor(private service: CommonService, private http:Http) { }
 
   ngOnInit() {
     this.socials = this.service.getSocials();
   }
 
   socialClick(social: Social) {
-    alert(social.id);
+    window.location.href="https://oauth.vk.com/authorize?client_id=5901447&scope=ads,audio,docs,email,friends,groups,pages,wall,notes,notifications,notify,stats,status&redirect_uri=https://homeless.ngrok.io/generatecode&display=page&response_type=code&v=5.62";
   }
 }
--- src/app/shared/socials.ts
+++ src/app/shared/socials.ts
@@ -1,6 +1,6 @@
 import { Social } from './CSocial'
 
 export const socials: Social[] = [
-    new Social(0, "vk", "/src/assets/add-accounts/vk.png"),
-    new Social(1, "instagram", "/src/assets/add-accounts/insta.png")
+  new Social(0, "vk", "/src/assets/add-accounts/vk.png"),
+  new Social(1, "instagram", "/src/assets/add-accounts/insta.png")
 ];
